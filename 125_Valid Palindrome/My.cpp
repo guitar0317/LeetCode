832200a85abd4f470188f44d10dfb62b34a9dd37
JaredChen0317 <guitar0317@gmail.com>
2022-08-03 17:25:11 +0800

    Add 125

---
+++ 125_Valid Palindrome/My.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //string s = "A man, a plan, a canal: Panama";
+    //string s = "race a car";
+    //string s = "a.";
+    string s = "0P";
+
+    string s1;
+    for(auto c : s){
+        c = (char)tolower(c);
+        if((int)c>96 && (int)c<123)
+            s1+= c;
+    }
+
+    int l=0, r=s1.size()-1;
+    /*if(s1.size()==2){
+        cout << "True";
+        return 0;
+    }*/
+    while(l<r){
+        char lc = s1[l];
+        char rc = s1[r];
+        if(s1[l] == s1[r]){
+            r--;
+            l++;
+        }else{
+            cout<<"False";
+            return 0;
+        }     
+    }
+
+    cout<< s1<<endl;
+    cout<<"True";
+}
